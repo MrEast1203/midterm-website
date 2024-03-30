26966f93555e2f927f586d88330dcc3a8f6ea85e
Kan <mreast1203@gmail.com>
2024-03-30 23:39:06 +0800

    try railway

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,3 +1,14 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <h1>
+      Originally from Hong Kong🇭🇰, I moved to Taiwan🇹🇼 since my university years
+      and am currently pursuing a degree in Electrical Engineering at National
+      Taiwan University. My enthusiasm for coding has driven me to engage in a
+      variety of projects, covering areas such as Unity, website development,
+      Raspberry Pi, and blockchain technologies. In addition to my academic and
+      coding endeavors, I have explored other interests by joining the AK Poker
+      Club🃏 and Japanese Mahjong Club🀄️, reflecting my curiosity and eagerness
+      to immerse myself in different cultures and activities.
+    </h1>
+  );
 }
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -59,8 +59,7 @@ export default function RootLayout() {
                                 : "text-gray-300 hover:bg-gray-700 hover:text-white",
                               "rounded-md px-3 py-2 text-sm font-medium"
                             )
-                          }
-                        >
+                          }>
                           {item.name}
                         </NavLink>
                       ))}
@@ -70,8 +69,7 @@ export default function RootLayout() {
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                   <button
                     type="button"
-                    className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  >
+                    className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                     <span className="sr-only">View notifications</span>
                     <BellIcon className="h-6 w-6" aria-hidden="true" />
                   </button>
@@ -95,8 +93,7 @@ export default function RootLayout() {
                       enterTo="transform opacity-100 scale-100"
                       leave="transition ease-in duration-75"
                       leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
+                      leaveTo="transform opacity-0 scale-95">
                       <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <Menu.Item>
                           {({ active }) => (
@@ -105,8 +102,7 @@ export default function RootLayout() {
                               className={classNames(
                                 active ? "bg-gray-100" : "",
                                 "block px-4 py-2 text-sm text-gray-700"
-                              )}
-                            >
+                              )}>
                               Your Profile
                             </a>
                           )}
@@ -118,8 +114,7 @@ export default function RootLayout() {
                               className={classNames(
                                 active ? "bg-gray-100" : "",
                                 "block px-4 py-2 text-sm text-gray-700"
-                              )}
-                            >
+                              )}>
                               Settings
                             </a>
                           )}
@@ -131,8 +126,7 @@ export default function RootLayout() {
                               className={classNames(
                                 active ? "bg-gray-100" : "",
                                 "block px-4 py-2 text-sm text-gray-700"
-                              )}
-                            >
+                              )}>
                               Sign out
                             </a>
                           )}
@@ -160,8 +154,7 @@ export default function RootLayout() {
                           "block rounded-md px-3 py-2 text-base font-medium"
                         )
                       }
-                      onClick={close}
-                    >
+                      onClick={close}>
                       {item.name}
                     </NavLink>
                   ))}
@@ -188,6 +181,7 @@ export function RootIndex() {
         <a href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
           <img src="/vite.svg" className="w-52 logo" />
         </a>
+        <h1>B09901135 Lab05</h1>
       </div>
     </div>
   );
